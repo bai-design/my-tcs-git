f5b8160e6fd3bac53847e803a748f48e2d8bc954
bai-design <1710448461@qq.com>
2020-05-09 11:28:00 +0800

    生成二维码图片功能添加注释

--- print_images/print_image.py
+++ print_images/print_image.py
@@ -2,16 +2,31 @@ import qrcode
 
 
 class Print_Image(object):
+    """实现生成二维码图片功能"""
     def __init__(self, org, name, telephone, email):
+        """
+            构造函数，初始化以下参数：
+
+            self.org：部门（职位）
+            self.name：姓名
+            self.telephone：电话
+            self.email: 邮箱
+
+        """
         self.org = org
         self.name = name
         self.telephone = telephone
         self.email = email
 
     def get_vard(self, path):
+        """生成二维码图片"""
+        # 构造vCard格式字符串
         template = "BEGIN:VCARD\nORG:{}\nFN:{}\nTEL:{}\nEMAIL:{}\nEND:VCARD" \
             .format(self.org, self.name, self.telephone, self.email)
         print(template)
+        # 制作二维码图片
         img = qrcode.make(template)
+        # 图片保存路径
         image_path = path + '%s.png'%(self.name)
+        # 保存图片
         img.save(image_path)
--- print_images/test_print.py
+++ print_images/test_print.py
@@ -13,14 +13,21 @@ if __name__ == '__main__':
     sheet_name_xls_00 = 'dkd-alphone'
     #创建读取实列
     read_book = Read_excle_xls(book_name_xls,sheet_name_xls_00)
-    #
+    # 调用read_excle_rows_xls（）返回 返回字典 key值为行数 value值为行内容
     image_lists = read_book.read_excle_rows_xls()
+    # 获取每行每列值
     for i in range(1, len(image_lists)):
+        # 部门（职位）
         im_org = image_lists[str(i)][0]
+        # 姓名
         im_name = image_lists[str(i)][1]
+        # 电话
         im_telephone = image_lists[str(i)][3]
+        # 邮箱
         im_email = image_lists[str(i)][4]
+        # 创建 Print_Image（）实列
         one_person = Print_Image(im_org, im_name, im_telephone, im_email)
+        # 调用get_vard（）方式生成个人信息二维码图片
         one_person.get_vard(image_path)
 
 
